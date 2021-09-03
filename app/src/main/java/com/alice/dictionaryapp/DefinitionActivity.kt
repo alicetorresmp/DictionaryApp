92f245cfa3964b61854023e371be2540c9367c91
Alice Torres <alicetorresmp@gmail.com>
2021-09-03 16:23:40 -0300

    showing its definition

--- app/src/main/java/com/alice/dictionaryapp/DefinitionActivity.kt
+++ app/src/main/java/com/alice/dictionaryapp/DefinitionActivity.kt
@@ -2,10 +2,20 @@ package com.alice.dictionaryapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alice.dictionaryapp.databinding.ActivityDefinitionBinding
+import com.alice.dictionaryapp.databinding.ActivityMainBinding
 
 class DefinitionActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityDefinitionBinding
+    private val KEY = "WORD_DEFINITION"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_definition)
+        binding = ActivityDefinitionBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.definitionTextView.text = intent.getStringExtra(KEY)
     }
 }
--- app/src/main/java/com/alice/dictionaryapp/MainActivity.kt
+++ app/src/main/java/com/alice/dictionaryapp/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.alice.dictionaryapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.alice.dictionaryapp.databinding.ActivityMainBinding
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import org.json.JSONArray
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
+    private val KEY = "WORD_DEFINITION"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,25 +24,33 @@ class MainActivity : AppCompatActivity() {
 
         val queue = Volley.newRequestQueue(this)
 
-        binding.findButton.setOnClickListener{
+        binding.findButton.setOnClickListener {
             val word = binding.wordEditText.text
             val apiKey = "75908353-c571-4e31-bd82-bc2552e570df"
             val url =
                 "https://dictionaryapi.com/api/v3/references/learners/json/$word?key=$apiKey"
 
             val stringRequest = StringRequest(Request.Method.GET, url,
-                Response.Listener{response ->
-                    response
+                { response ->
+                    extractDefinitionFromJson(response)
                 },
-                Response.ErrorListener {error ->
-                    error
+                { error ->
+                    Toast.makeText(this, error.message, Toast.LENGTH_SHORT).show()
                 }
             )
             queue.add(stringRequest)
         }
     }
 
-    private fun extractDefinitionFromJson(response : String){
+    private fun extractDefinitionFromJson(response: String) {
 
+        val jsonArray = JSONArray(response)
+        val firstIndex = jsonArray.getJSONObject(0)
+        val getShortDefinition = firstIndex.getJSONArray("shortdef")
+        val firstShortDefinition = getShortDefinition.get(0)
+
+        val intent = Intent(this, DefinitionActivity::class.java)
+        intent.putExtra(KEY, firstShortDefinition.toString())
+        startActivity(intent)
     }
-}
+    }
